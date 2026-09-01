1aa71511a0bdd889b594d921e41abcb406db0d13
Jonathan <john.ramirezaws@gmail.com>
2026-09-01 16:21:22 -0400

    Agrega busqueda y filtros de mascotas

--- pacientes/views.py
+++ pacientes/views.py
@@ -5,8 +5,24 @@ from .models import Mascota
 def listar_mascotas(request):
     mascotas = Mascota.objects.all()
 
+    busqueda = request.GET.get('nombre')
+    especie = request.GET.get('especie')
+    vacunacion = request.GET.get('vacunacion')
+
+    if busqueda:
+        mascotas = mascotas.filter(nombre__icontains=busqueda)
+
+    if especie:
+        mascotas = mascotas.filter(especie__iexact=especie)
+
+    if vacunacion:
+        mascotas = mascotas.filter(vacunacion=vacunacion)
+
     contexto = {
-        'mascotas': mascotas
+        'mascotas': mascotas,
+        'busqueda': busqueda,
+        'especie': especie,
+        'vacunacion': vacunacion
     }
 
     return render(request, 'pacientes/listar.html', contexto)
