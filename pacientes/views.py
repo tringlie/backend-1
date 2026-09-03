c9243ca80b7e61066b2c8e1eccf55516794a421f
tringlie <benjmain.jofre@gmail.com>
2026-09-03 18:54:41 -0400

    implementacion busqueda por nombre en /mascotas ademas se corrige el error de color por estado vacuna

--- pacientes/views.py
+++ pacientes/views.py
@@ -5,16 +5,23 @@ def listar_mascotas(request):
     mascotas = Mascota.objects.all()
 
     especies = Mascota.objects.values_list('especie', flat=True).distinct()
+    busqueda = request.GET.get('busqueda')
 
     especie_filtrada = request.GET.get('especie')
 
     if especie_filtrada:
         mascotas = mascotas.filter(especie=especie_filtrada)
 
+    if busqueda:
+        busqueda = busqueda.strip()
+        if busqueda:
+            mascotas = mascotas.filter(nombre__icontains=busqueda)
+
     contexto = {
         'mascotas': mascotas,
         'especies': especies,
-        'especie_filtrada': especie_filtrada
+        'especie_activa': especie_filtrada or '',
+        'busqueda': busqueda or '',
     }
 
 
