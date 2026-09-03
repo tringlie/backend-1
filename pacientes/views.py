481ef3b62e9185efb30b36761d87667a7b072ca0
tringlie <benjmain.jofre@gmail.com>
2026-09-03 18:41:28 -0400

    implementacion filtro por especie en vista /mascotas

--- pacientes/views.py
+++ pacientes/views.py
@@ -3,4 +3,19 @@ from .models import Mascota
 
 def listar_mascotas(request):
     mascotas = Mascota.objects.all()
-    return render(request, 'listar.html', {'mascotas': mascotas})
+
+    especies = Mascota.objects.values_list('especie', flat=True).distinct()
+
+    especie_filtrada = request.GET.get('especie')
+
+    if especie_filtrada:
+        mascotas = mascotas.filter(especie=especie_filtrada)
+
+    contexto = {
+        'mascotas': mascotas,
+        'especies': especies,
+        'especie_filtrada': especie_filtrada
+    }
+
+
+    return render(request, 'listar.html', contexto)
